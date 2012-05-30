633617205c9e8e7c7277e8014e10f357bf715665
haches <estlerh@inf.ethz.ch>
2012-05-30 10:22:38 +0200

    Intermediate step of annotator tutorial.
    Highlighting and clicking is working now.

---
+++ annotator/data/selector.js
@@ -0,0 +1,48 @@
+var matchedElement = null;
+var originalBgColor = null;
+var active = false;
+
+function resetMatchedElement() {
+	if (matchedElement) {
+		$(matchedElement).css('background-color', originalBgColor);
+		$(matchedElement).unbind('click.annotator');
+	}
+}
+
+self.on('message', function onMessage(activation) {
+	active = activation;
+	if (!active) {
+		resetMatchedElement();
+	}
+});
+
+$('*').mouseenter(function () {	
+	if (!active || $(this).hasClass('annotated')) {
+		return;
+	}
+
+	resetMatchedElement();
+	ancestor = $(this).closest('[id]');
+	matchedElement = $(this).first();
+
+	originalBgColor = $(matchedElement).css('background-color');
+	$(matchedElement).css('background-color', 'yellow');
+	$(matchedElement).bind('click.annotator', function(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		
+		console.log("About to emit a 'show'");
+		
+		self.port.emit('show',
+			[
+				document.location.toString(),
+				$(ancestor).attr("id"),
+				$(matchedElement).text()
+			]
+		);
+	});
+});
+
+$('*').mouseout(function() {
+	resetMachtedElement();
+});
--- annotator/lib/main.js
+++ annotator/lib/main.js
@@ -1,11 +1,28 @@
 var widgets = require("widget");
+var pageMod = require("page-mod");
 var data = require("self").data;
+var selectors = [];
 
 var annotatorIsOn = false;
 
+function activateSelectors() {
+	selectors.forEach(
+		function (selector) {
+			selector.postMessage(annotatorIsOn);
+	});
+}
+
 function toggleActivation() {
 	annotatorIsOn = !annotatorIsOn;
+	activateSelectors();
 	return annotatorIsOn;
+}
+
+function detachWorker(worker, workerArray) {
+	var index = workerArray.indexOf(worker);
+	if(index != -1) {
+		workerArray.splice(index, 1);
+	}
 }
 
 exports.main = function() {
@@ -27,5 +44,22 @@ exports.main = function() {
 	
 	widget.port.on("right-click", function() {
 		console.log("show annotation list");
+	});
+	
+	var selector = pageMod.PageMod({
+	  include: ['*'],
+	  contentScriptWhen: 'ready',
+	  contentScriptFile: [data.url('jquery-1.7.2.min.js'),
+	                      data.url('selector.js')],
+	  onAttach: function(worker) {
+	    worker.postMessage(annotatorIsOn);
+	    selectors.push(worker);
+	    worker.port.on('show', function(data) {
+	      console.log(data);
+	    });
+	    worker.on('detach', function () {
+	      detachWorker(this, selectors);
+	    });
+	  }
 	});
 }
